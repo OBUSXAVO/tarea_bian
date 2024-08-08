e2f11a6119fe7eab88c670dcf8aaf8a867ced0d1
OBUSXAVO <obusxavo@gmail.com>
2024-08-08 10:58:25 -0500

    subir plataforma completo

--- employee_data_management/app.py
+++ employee_data_management/app.py
@@ -9,24 +9,19 @@ api = Api(app)
 swagger = Swagger(app)
 
 class RetrievePerson(Resource):
-    @swag_from('swagger.yml')
+    @swag_from('swagger3.yml')
     def post(self):
         # Obtener datos del cuerpo de la solicitud
-        data = request.get_json()
-        
-
+        data = request.get_json() 
         # Validar los datos de la solicitud (esto se puede mejorar con validaciones adicionales)
         if 'PersonIdentification' not in data:
             return jsonify({
                 "status_code": "400",
                 "status": "BadRequest",
                 "message": "Missing PersonIdentification in request"
-            }), 400
-
-        
-        internal_data = Converter.request_to_internal(data)
-
-        #aui vendria la respuesta de una api externa se entra a simular
+            }), 400        
+        internal_data = Converter.request_to_internal_3(data)
+        #aqui vendria la respuesta de una api externa se entra a simular
         internal_response = {
             "documentType": internal_data['documentType'],
             "documentNumber": internal_data['documentNumber'],
@@ -35,12 +30,57 @@ class RetrievePerson(Resource):
             "mobile_phone_number": "123456789",
             "personal_email_address": "obusxavo@gmail.com"
         }
-
-        response = Converter.internal_to_response(internal_response)
-
+        response = Converter.internal_to_response_3(internal_response)
+        return jsonify(response)
+    
+class RetrievePersonAccount(Resource):
+    @swag_from('swagger1.yml')
+    def post(self):
+        # Obtener datos del cuerpo de la solicitud
+        data = request.get_json() 
+        # Validar los datos de la solicitud (esto se puede mejorar con validaciones adicionales)
+        if 'PersonIdentification' not in data:
+            return jsonify({
+                "status_code": "400",
+                "status": "BadRequest",
+                "message": "Missing PersonIdentification in request"
+            }), 400
+        internal_data = Converter.request_to_internal_1(data)
+        #aqui vendria la respuesta de una api externa se entra a simular
+        internal_response = {
+            "id_cuenta": "123456789",
+            "tipo_moneda": "USD"
+        }
+        response = Converter.internal_to_response_1(internal_response)
+        return jsonify(response)
+    
+class RetrieveAccount(Resource):
+    @swag_from('swagger2.yml')
+    def post(self):
+        # Obtener datos del cuerpo de la solicitud
+        data = request.get_json() 
+        # Validar los datos de la solicitud (esto se puede mejorar con validaciones adicionales)
+        if 'Account' not in data:
+            return jsonify({
+                "status_code": "400",
+                "status": "BadRequest",
+                "message": "Missing PersonIdentification in request"
+            }), 400        
+        internal_data = Converter.request_to_internal_2(data)
+        #aqui vendria la respuesta de una api externa se entra a simular
+        internal_response = {
+            "numero_cuenta": internal_data['numero_cuenta'],
+            "tipo_moneda_cuenta": "USD",
+            "estado_cuenta": "active",
+            "tipo_cuenta": "saving"
+        }
+        response = Converter.internal_to_response_2(internal_response)
         return jsonify(response)
 
+
 api.add_resource(RetrievePerson, '/employee-data-management/v1.0/retrieve')
+api.add_resource(RetrievePersonAccount, '/savings-account/v1.0/initiate')
+api.add_resource(RetrieveAccount, '/savings-account/v1.0/retrieve')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- employee_data_management/services/converter.py
+++ employee_data_management/services/converter.py
@@ -1,6 +1,6 @@
 class Converter:
     @staticmethod
-    def request_to_internal(data):
+    def request_to_internal_3(data):
         """
         transformation BIAN -> no BIAN
         """
@@ -11,7 +11,7 @@ class Converter:
         return internal_data
 
     @staticmethod
-    def internal_to_response(internal_data):
+    def internal_to_response_3(internal_data):
         """
         transformation no BIAN -> BIAN
         """
@@ -27,6 +27,55 @@ class Converter:
                     "MobilePhoneNumber": internal_data.get("mobile_phone_number", ""),
                     "PersonalEmailAddress": internal_data.get("personal_email_address", "")
                 }
+            }
+        }
+        return response_data
+    
+    @staticmethod
+    def request_to_internal_1(data):
+        """
+        transformation BIAN -> no BIAN
+        """
+        internal_data = {
+            "tipo_documento": data["PersonIdentification"]["TypeOfIdentification"],
+            "numero_documento": data["PersonIdentification"]["IdentityCardNumber"]
+        }
+        return internal_data
+
+    @staticmethod
+    def internal_to_response_1(internal_data):
+        """
+        transformation no BIAN -> BIAN
+        """
+        response_data = {
+            "Account": {
+                "Identification": internal_data.get("id_cuenta", ""),
+                "BaseCurrency": internal_data.get("tipo_moneda", "")
+            }
+        }
+        return response_data
+    
+    @staticmethod
+    def request_to_internal_2(data):
+        """
+        transformation BIAN -> no BIAN
+        """
+        internal_data = {
+            "numero_cuenta": data["Account"]["Identification"]
+        }
+        return internal_data
+
+    @staticmethod
+    def internal_to_response_2(internal_data):
+        """
+        transformation no BIAN -> BIAN
+        """
+        response_data = {
+            "Account": {
+                "Identification": internal_data.get("numero_cuenta", ""),
+                "BaseCurrency": internal_data.get("tipo_moneda_cuenta", ""),
+                "Status": internal_data.get("estado_cuenta", ""),
+                "Type": internal_data.get("tipo_cuenta", "")
             }
         }
         return response_data
